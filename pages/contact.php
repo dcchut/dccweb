68d9fa09882b6a2efc8756ddcce0e6874143917f
robbo <dcchut@gmail.com>
2011-04-26 20:33:18 +1000

    fixed some shit

--- pages/contact.php
+++ pages/contact.php
@@ -11,7 +11,7 @@ function get_page_content()
 	ob_start();
 	?>
 	<div class="mheading">contact me</div>
-	<p class="lspace"><b>email</b>: dcc at gee mail dot com<br />
+	<p class="lspace"><b>email</b>: dcchut at gee mail dot com<br />
 	<b>github</b>: <a href="https://github.com/dcchut">dcchut</a><br />
 	<b>irc</b>: <i>robbo</i> on <i>irc.freenode.net</i> and <i>irc.esper.net</i><br />
 	<b>snail mail</b>: nah</p>
