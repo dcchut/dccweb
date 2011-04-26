db5556516e9c360dfc3c9281da092a0fa079ff52
robbo <dcchut@gmail.com>
2011-04-27 00:10:55 +1000

    update bio

--- pages/bio.php
+++ pages/bio.php
@@ -12,8 +12,22 @@ function get_page_content()
 	?>
 	<div class="mheading">my bio</div>
 	
-	<p class="lspace">think of something interesting here.</p>
+	<p class="lspace">hi, I'm Rob, a <?php echo get_age(); ?> year old student studying <a href="http://ms.unimelb.edu.au">mathematics</a> as an undergraduate at the <a href="http://www.unimelb.edu.au">university of melbourne</a>.
+	I enjoy programming in PHP, Python and C# and I am actively interested in learning new things and being the best that I can be.  
+	in my spare time, I enjoy playing the electric bass for <a href="http://www.myspace.com/the.rapalas">The Rapalas</a>, playing Civilization V and learning about computer science.</p>
 	
 	<?php
 	return ob_get_clean();
+}
+
+function get_age()
+{
+	$y = date("Y") - 1991;
+	$m = date("m") - 2;
+	$d = date("d") - 14;
+	
+	if ($m < 0) $y--;
+	elseif ($m == 0 && $d < 0) $y--;
+	
+	return $y;
 }
