46627b6a3c4f296783d91e7824a2007d1ad3f385
robbo <dcchut@gmail.com>
2011-06-07 22:41:14 +1000

    added NS to repo

---
+++ dccweb/Core/NS.php
@@ -0,0 +1,13 @@
+<?php
+namespace dccweb\Core;
+
+class NS {
+  protected $ns;
+  public function __construct(array $ns) {
+    $this->ns = $ns;
+  }
+  
+  public function Get(){
+    return implode('\\', $this->ns) . '\\';
+  }
+}
