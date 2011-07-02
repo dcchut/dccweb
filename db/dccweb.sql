60bf8c6c03a10a40677fb6055e541354a12e6e19
robbo <dcchut@gmail.com>
2011-07-02 16:35:28 +1000

    added schema file to /db/ folder

---
+++ db/dccweb.sql
@@ -0,0 +1,13 @@
+-- dccweb schema file
+
+CREATE TABLE posts (
+    `id` INTEGER PRIMARY KEY,
+    `o` INTEGER,
+    `content` TEXT
+);
+
+CREATE TABLE projects (
+    `id` INTEGER PRIMARY KEY,
+    `o` INTEGER,
+    `content` TEXT
+);
