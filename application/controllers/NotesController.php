2b6eabdb0c026378307df91c51a0854d673f03e7
robbo <dcchut@gmail.com>
2012-03-24 17:39:07 +1100

    Added notes page

---
+++ application/controllers/NotesController.php
@@ -0,0 +1,16 @@
+<?php
+
+class ProjectsController extends Zend_Controller_Action
+{
+    public function init()
+    {
+        $ajaxContext = $this->_helper->getHelper('AjaxContext');
+        $ajaxContext->addActionContext('index', 'html')
+                    ->initContext();
+    }
+
+    public function indexAction()
+    {
+    }
+}
+
